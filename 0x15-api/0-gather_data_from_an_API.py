0eab699d8818825165238883bf2d68ff81316c88
Michael Kanu <michaelokanu01@yahoo.com>
2024-05-20 15:39:55 -0700

    todo-api1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" A python script to fetch data from API
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url+'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url+'todos/?userId={}'.format(sys.argv[1]))
+    todo_all = todos.json()
+    completed = []
+    for todo in todo_all:
+        if todo.get("completed") == True:
+            completed.append(todo)
+
+    print("Employee {} is done with tasks({}/{}):".format(users.
+          get("name"),len(completed),len(todo_all)))
+
+    #print(completed)
+    for t in completed:
+        print("\t {}".format(t.get("title")))
