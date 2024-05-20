76b5bba9180bd2e0bb7e589879632b51267b9ae0
Michael Kanu <michaelokanu01@yahoo.com>
2024-05-20 16:32:13 -0700

    api-to-json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script to copy api response to file
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(u)
+    users = requests.get(url).json()
+    user_name = users.get("username")
+    todos = requests.get(url + '/todos').json()
+
+    data_dict = {u: []}
+    for t in todos:
+        t_title = t.get("title")
+        t_status = t.get("completed")
+        data_dict[u].append({"task": "{}".format(t_title),
+            "completed": t_status, "username": "{}".format(user_name)})
+
+    with open("{}.json".format(u), "w") as json_file:
+        json.dump(data_dict, json_file)
