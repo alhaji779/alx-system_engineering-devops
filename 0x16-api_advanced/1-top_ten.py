6da3545f9fbfa8709f266fc5e766e963d167c964
Michael Kanu <michaelokanu01@yahoo.com>
2024-06-04 15:36:01 -0700

    api-reddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@
 from sys import argv
 import requests
 
+
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
